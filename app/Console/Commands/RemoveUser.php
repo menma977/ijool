60e3f901fc960680ec9131d164b2b3be624a940d
Smity Waber Jager Man Janson <38025704+menma977@users.noreply.github.com>
2021-03-12 20:38:04 +0700

    Update RemoveUser.php

--- app/Console/Commands/RemoveUser.php
+++ app/Console/Commands/RemoveUser.php
@@ -38,15 +38,15 @@ class RemoveUser extends Command
   {
     $user = User::whereNull("email_verified_at")->where("created_at", ">=", Carbon::now()->subDay())->first();
     if ($user) {
-      Doge::deleted($user->id);
-      Trading::deleted($user->id);
-      Profile::deleted($user->id);
-      Subscribe::where("user_id", $user->id)->deleted();
-      Queue::where("from", $user->id)->orWhere("to", $user->id)->deleted();
-      Bill::where("from", $user->id)->orWhere("to", $user->id)->deleted();
-      Line::where("user_id", $user->id)->orWhere("mate", $user->id)->deleted();
-      Permission::where("user_id", $user->id)->deleted();
-      $user->deleted();
+      Doge::destroy($user->id);
+      Trading::destroy($user->id);
+      Profile::destroy($user->id);
+      Subscribe::where("user_id", $user->id)->delete();
+      Queue::where("from", $user->id)->orWhere("to", $user->id)->delete();
+      Bill::where("from", $user->id)->orWhere("to", $user->id)->delete();
+      Line::where("user_id", $user->id)->orWhere("mate", $user->id)->delete();
+      Permission::where("user_id", $user->id)->delete();
+      $user->delete();
     }
   }
 }
